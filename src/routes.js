22ed784fa6a6ef526a7b37947f4f2d1e5e2db776
Yago Gomes <yagogomesc@gmail.com>
2021-04-06 09:17:13 -0300

    Finished CRUD of pages and adjusted html

--- src/routes.js
+++ src/routes.js
@@ -72,7 +72,7 @@ const Job = {
           ...job,
           remaining,
           status,
-          budget: Profile.data["value-hour"] * job["total-hours"],
+          budget: Job.services.calculateBudget(job, Profile.data["value-hour"]),
         };
       });
 
@@ -86,7 +86,7 @@ const Job = {
     },
     save(req, res) {
       // req.body = {name: '', 'daily-hours':'', 'total-hours': ''}
-      const lastId = Job.data[Job.data.length - 1]?.id || 1;
+      const lastId = Job.data[Job.data.length - 1]?.id || 0;
 
       Job.data.push({
         id: lastId + 1,
@@ -104,7 +104,43 @@ const Job = {
       if (!job) {
         return res.send("Job not found!");
       }
+
+      job.budget = Job.services.calculateBudget(
+        job,
+        Profile.data["value-hour"]
+      );
+
       return res.render(views + "job-edit", { job });
+    },
+    update(req, res) {
+      const jobId = req.params.id;
+      const job = Job.data.find((job) => Number(job.id) === Number(jobId));
+      if (!job) {
+        return res.send("Job not found!");
+      }
+
+      const updatedJob = {
+        ...job,
+        name: req.body.name,
+        "total-hours": req.body["total-hours"],
+        "daily-hours": req.body["daily-hours"],
+      };
+
+      Job.data = Job.data.map((job) => {
+        if (Number(job.id) === Number(jobId)) {
+          job = updatedJob;
+        }
+
+        return job;
+      });
+
+      res.redirect("/job/" + jobId);
+    },
+    delete(req, res) {
+      const jobId = req.params.id;
+      Job.data = Job.data.filter((job) => Number(job.id) !== Number(jobId));
+
+      return res.redirect("/");
     },
   },
   services: {
@@ -122,6 +158,7 @@ const Job = {
 
       return dayDiff;
     },
+    calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
   },
 };
 
@@ -131,6 +168,8 @@ routes.get("/job", Job.controllers.create);
 routes.post("/job", Job.controllers.save);
 
 routes.get("/job/:id", Job.controllers.show);
+routes.post("/job/:id", Job.controllers.update);
+routes.post("/job/delete/:id", Job.controllers.delete);
 
 routes.get("/profile", Profile.controllers.index);
 routes.post("/profile", Profile.controllers.update);
