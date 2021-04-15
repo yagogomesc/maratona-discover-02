bdc2c04c8bf03da0116c8c8cf2a2441da39bfda0
Yago Gomes <yagogomesc@gmail.com>
2021-04-14 22:00:18 -0300

    Adjusted to show total free hours or dont have free hours

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -4,22 +4,32 @@ const Profile = require("../model/Profile");
 
 module.exports = {
   index(req, res) {
+    const job = Job.get();
     let statusCount = {
       progress: 0,
       done: 0,
-      total: Job.get().length,
+      total: job.length,
     };
 
     // Total de horas por dia de cada job em progresso
     let jobTotalHours = 0;
 
-    const updatedJobs = Job.get().map((job) => {
+    const updatedJobs = job.map((job) => {
       // Ajustes no jobs
       // calculo de tempo restante
       const remaining = JobUtils.remainingDays(job);
 
       const status = remaining <= 0 ? "done" : "progress";
       statusCount[status] += 1;
+
+      // Total de horas por dia de cada job em progresso
+      jobTotalHours =
+        status == "progress"
+          ? jobTotalHours + Number(job["daily-hours"])
+          : jobTotalHours;
+      // if (status === "progress") {
+      //   jobTotalHours += Number(job["daily-hours"]);
+      // }
 
       return {
         ...job,
